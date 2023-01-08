a8f89f17b0e0411375a67ac8e43ebc8171bcc41d
RoMTHoS <matheosromain@gmail.com>
2023-01-08 19:44:33 +0100

    Add sign in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import SignIn from "./SignIn";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Auth Template with React and Tailwind</h1>
+        <SignIn />
       </header>
     </div>
   );
